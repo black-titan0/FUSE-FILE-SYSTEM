9d694d176c6a73bfb526e9dfca6e51aec037df70
AliSafara <ali.safarafard@gmail.com>
2024-02-01 03:18:50 +0330

    added lock mechanism.

--- Passthrough.py
+++ Passthrough.py
@@ -32,7 +32,7 @@ class Passthrough(Operations):
         cursor = connector.cursor()
         print("Path:", path)
         hashed_path = self.generate_hash(path)
-        
+
         # print("Hashed path:", hashed_path)
         cursor.execute('''
             INSERT INTO files (name, hashed_name, is_locked)
@@ -48,32 +48,54 @@ class Passthrough(Operations):
     def readdir(self, path, fh):
         dirs = set()
         for p in [self.primary, self.fallback]:
-            try:
-                dirs.update(os.listdir(p + path))
-            except FileNotFoundError:
-                pass
+            full_path = os.path.join(p, path.lstrip("/"))
+            if os.path.exists(full_path):
+                try:
+                    for item in os.listdir(full_path):
+                        item_full_path = os.path.join(full_path, item)
+                        if os.path.isfile(item_full_path):
+                            hashed_name = self.generate_hash(item_full_path)
+                            connector = sqlite3.connect('files.db')
+                            cursor = connector.cursor()
+                            try:
+                                cursor.execute('''
+                                    INSERT INTO files (name, hashed_name, is_locked)
+                                    VALUES (?, ?, ?)
+                                ''', (item_full_path, hashed_name, 0))
+                                connector.commit()  # Don't forget to commit the changes
+                            except sqlite3.IntegrityError:
+                                pass
+                            connector.close()
+                        dirs.add(item)
+                except FileNotFoundError:
+                    pass
         return ['.', '..'] + list(dirs)
 
     def read(self, path, size, offset, fh):
+        if not self.acquire_file_lock(path):
+            return -1
         full_path = self._full_path(path)
         if not os.path.exists(full_path):
             full_path = self._full_path(path, primary=False)
 
         with open(full_path, 'rb') as f:
             f.seek(offset)
-            return f.read(size)
+            content = f.read(size)
+            self.release_file_lock(path)
+            return content
 
     def write(self, path, buf, offset, fh):
+        if not self.acquire_file_lock(path):
+            return -1
         full_path = self._full_path(path)
         if not os.path.exists(full_path):
             full_path = self._full_path(path, primary=False)
 
-        # Open the file in binary read-write mode, don't truncate (r+b)
-        # If the file doesn't exist, open in write mode which will create the file (wb)
         mode = 'r+b' if os.path.exists(full_path) else 'wb'
         with open(full_path, mode) as f:
             f.seek(offset)
             written = f.write(buf)
+            self.release_file_lock(path)
             return written
 
     def unlink(self, path):
@@ -85,26 +107,48 @@ class Passthrough(Operations):
         ''', (path,))
         connector.commit()
         connector.close()
-        
+
         primary_path = self._full_path(path)
         fallback_path = self._full_path(path, primary=False)
 
-        # Check if the file exists in the primary filesystem
         if os.path.exists(primary_path):
             os.unlink(primary_path)
         elif os.path.exists(fallback_path):
-            # Optionally handle the case where the file is in the fallback filesystem
             os.unlink(fallback_path)
         else:
             raise FuseOSError(errno.ENOENT)
-        
+
     def generate_hash(self, value):
         hash_algorithm = hashlib.sha256()
         value_bytes = str(value).encode('utf-8')
         hash_algorithm.update(value_bytes)
         hash_value = hash_algorithm.hexdigest()
 
-        return hash_value    
+        return hash_value
+
+    def acquire_file_lock(self, path):
+        hashed_path = self.generate_hash(path)
+        with sqlite3.connect('files.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT is_locked FROM files WHERE hashed_name = ?", (hashed_path,))
+            result = cursor.fetchone()
+
+            if result is None:
+                pass
+            elif result[0] == 1:
+                print("Error: The file is currently in use by someone else, try again later.")
+                return False
+            cursor.execute("UPDATE files SET is_locked = 1 WHERE hashed_name = ?", (hashed_path,))
+            conn.commit()
+            return True
+
+    def release_file_lock(self, path):
+        hashed_path = self.generate_hash(path)
+        with sqlite3.connect('files.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE files SET is_locked = 0 WHERE hashed_name = ?", (hashed_path,))
+            conn.commit()
+
 
 def main(mountpoint, primary, fallback):
     username = os.getlogin()
@@ -117,7 +161,7 @@ def main(mountpoint, primary, fallback):
         CREATE TABLE IF NOT EXISTS files (
             "id" INTEGER PRIMARY KEY,
             "name" TEXT,
-            "hashed_name" TEXT,       
+            "hashed_name" TEXT UNIQUE,       
             "is_locked" INTEGER
         )
     ''')
@@ -126,6 +170,7 @@ def main(mountpoint, primary, fallback):
     connector.close()
 
     FUSE(Passthrough(primary, fallback), mountpoint, nothreads=True, foreground=True)
+
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], sys.argv[3])
